3eea25cbb1f84c2a969d33369284e24b786668c4
Lam Nhan <lamnhan92@gmail.com>
2019-09-27 09:11:32 +0700

    add rewiremock nodejs

--- src/rewiremock.ts
+++ src/rewiremock.ts
@@ -1,5 +1,6 @@
-import rewiremock from 'rewiremock';
+import rewiremock, { addPlugin, plugins } from 'rewiremock';
 
 rewiremock.overrideEntryPoint(module);
+addPlugin(plugins.nodejs);
 
 export { rewiremock };
